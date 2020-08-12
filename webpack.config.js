48bd1427f996d50e1c61a74a8e84130a0f737ea9
kwizl <karis.njoroge@gmail.com>
2020-08-12 13:34:23 +0300

    Finish mockup html and css design

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: './js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
